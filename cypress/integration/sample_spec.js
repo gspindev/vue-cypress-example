707cfcbb33b13cd06f846fdd81014895b30e4ac1
gspindev <pradeep.sanjeevaraya@aa.com>
2022-01-26 08:04:17 -0600

    Update sample_spec.js

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -1,5 +1,16 @@
-describe('My First Test', () => {
-  it('Does not do much!', () => {
-    expect(true).to.equal(true)
-  })
-})
+import { shallowMount } from "@vue/test-utils";
+import HelloWorld from "@/components/HelloWorld.vue";
+
+describe("HelloWorld", () => {
+  const msg = "Vue Unit and E2E Test with Cypress";
+  it(`Title should display '${msg}'`, () => {
+    HelloWorld.components = HelloWorld.components || {};
+    let wrapper = shallowMount(HelloWorld, { propsData: { msg } });
+    expect(wrapper.find("h1").text()).eq(msg);
+  });
+  it(`Title should be empty`, () => {
+    HelloWorld.components = HelloWorld.components || {};
+    let wrapper = shallowMount(HelloWorld, {});
+    expect(wrapper.find("h1").text()).to.be.empty;
+  });
+});
